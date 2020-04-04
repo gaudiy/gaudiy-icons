9dbd2556ed6166f8c77389de70cfd371a4e71bad
kodai3 <k3dai.su3@gmail.com>
2020-04-04 21:39:17 +0900

    refactor with material ui core

--- src/SvgIcon/Presenter.tsx
+++ src/SvgIcon/Presenter.tsx
@@ -1,18 +1,36 @@
+import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import * as React from 'react';
 import { SvgIconProps } from '../typings';
-import './styles.scss';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '1em',
+    height: '1em',
+    color: 'currentColor',
+    userSelect: 'none',
+    fontAize: '24px',
+  },
+  fontSizeSmall: {
+    fontSize: '20px',
+  },
+  fontSizeLarge: {
+    fontSize: '36px',
+  },
+}));
 
 function capitalize(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 const SvgIcon = (props: SvgIconProps) => {
+  const classes = useStyles();
   const { children, className, viewBox = '0 0 24 24', fontSize = 'default', ...other } = props;
 
   return (
     <svg
-      className={clsx('root', `fontSize${capitalize(fontSize)}`, className)}
+      // @ts-ignore
+      className={clsx(classes.root, classes[`fontSize${capitalize(fontSize)}`], className)}
       focusable="false"
       viewBox={viewBox}
       fill="none"
