b9d40ba7cb10c90ebacad0f462f75ce01a211fe7
kodai3 <k3dai.su3@gmail.com>
2020-04-04 15:51:05 +0900

    babel config

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+let defaultPresets;
+
+// We release a ES version of Material-UI.
+// It's something that matches the latest official supported features of JavaScript.
+// Nothing more (stage-1, etc), nothing less (require, etc).
+if (process.env.BABEL_ENV === 'es') {
+  defaultPresets = [];
+} else {
+  defaultPresets = [
+    [
+      '@babel/preset-env',
+      {
+        bugfixes: true,
+        modules: ['esm', 'production-umd'].includes(process.env.BABEL_ENV) ? false : 'commonjs',
+      },
+    ],
+  ];
+}
+
+module.exports = {
+  presets: defaultPresets.concat(['@babel/preset-react', '@babel/preset-typescript']),
+  plugins: ['babel-plugin-optimize-clsx'],
+  env: {
+    esm: {
+      plugins: ['babel-plugin-optimize-clsx', ['@babel/plugin-transform-runtime', { useESModules: true }]],
+    },
+  },
+  ignore: ['**/*.d.ts'],
+};
---
+++ scripts/copy-files.js
@@ -0,0 +1,69 @@
+/* eslint-disable no-console */
+const path = require('path');
+const fse = require('fs-extra');
+const glob = require('glob');
+
+const packagePath = process.cwd();
+const buildPath = path.join(packagePath, './dist');
+const srcPath = path.join(packagePath, './src');
+
+/**
+ * Puts a package.json into every immediate child directory of rootDir.
+ * That package.json contains information about esm for bundlers so that imports
+ * like import Typography from '@material-ui/core/Typography' are tree-shakeable.
+ *
+ * It also tests that an this import can be used in typescript by checking
+ * if an index.d.ts is present at that path.
+ *
+ * @param {string} rootDir
+ */
+async function createModulePackages({ from, to }) {
+  const directoryPackages = glob.sync('*/index.js', { cwd: from }).map(path.dirname);
+
+  await Promise.all(
+    directoryPackages.map(async (directoryPackage) => {
+      const packageJson = {
+        sideEffects: false,
+        module: path.join('../esm', directoryPackage, 'index.js'),
+        typings: './index.d.ts',
+      };
+      const packageJsonPath = path.join(to, directoryPackage, 'package.json');
+
+      const [typingsExist] = await Promise.all([
+        fse.exists(path.join(to, directoryPackage, 'index.d.ts')),
+        fse.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2)),
+      ]);
+
+      if (!typingsExist) {
+        throw new Error(`index.d.ts for ${directoryPackage} is missing`);
+      }
+
+      return packageJsonPath;
+    })
+  );
+}
+
+async function typescriptCopy({ from, to }) {
+  if (!(await fse.exists(to))) {
+    console.warn(`path ${to} does not exists`);
+    return [];
+  }
+
+  const files = glob.sync('**/*.d.ts', { cwd: from });
+  const cmds = files.map((file) => fse.copy(path.resolve(from, file), path.resolve(to, file)));
+  return Promise.all(cmds);
+}
+
+async function run() {
+  try {
+    // TypeScript
+    await typescriptCopy({ from: srcPath, to: buildPath });
+
+    await createModulePackages({ from: srcPath, to: buildPath });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+run();
