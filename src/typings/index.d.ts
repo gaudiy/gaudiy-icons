c14a2581b6dd8fc50dedff25a276c53aee8b2884
kodai3 <k3dai.su3@gmail.com>
2020-04-11 20:05:28 +0900

    make type optional

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 export interface SvgIconProps extends React.SVGProps<SVGSVGElement> {
-  fontSize: 'default' | 'small' | 'large';
+  fontSize?: 'default' | 'small' | 'large';
 }
 
 export default function SvgIcon(props: SvgIconProps): JSX.Element;
