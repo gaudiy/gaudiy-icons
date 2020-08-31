c3a4470d0bcc3710099ea966d7c0fe563f643f7d
kodai3 <k3dai.su3@gmail.com>
2020-08-31 14:44:33 +0900

    prettier

--- src/Arrows/CornerDownRight.tsx
+++ src/Arrows/CornerDownRight.tsx
@@ -4,11 +4,6 @@ import createSvgIcon from '../utils/createSvgIcon';
 export default createSvgIcon(
   <>
     <path d="M15 10L20 15L15 20" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
-    <path
-      d="M4 4V11C4 13.2091 5.79086 15 8 15H20"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
+    <path d="M4 4V11C4 13.2091 5.79086 15 8 15H20" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
   </>
 );
--- src/Arrows/CornerUpRight.tsx
+++ src/Arrows/CornerUpRight.tsx
@@ -4,11 +4,6 @@ import createSvgIcon from '../utils/createSvgIcon';
 export default createSvgIcon(
   <>
     <path d="M15 14L20 9L15 4" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
-    <path
-      d="M4 20V13C4 10.7909 5.79086 9 8 9H20"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
+    <path d="M4 20V13C4 10.7909 5.79086 9 8 9H20" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
   </>
 );
