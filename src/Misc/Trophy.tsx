615bf98db4f2f02d33dcc5686ea4843ae0c38fa6
kodai3 <k3dai.su3@gmail.com>
2020-09-24 12:07:43 +0900

    update

--- src/Misc/Trophy.tsx
+++ src/Misc/Trophy.tsx
@@ -2,26 +2,28 @@ import * as React from 'react';
 import createSvgIcon from '../utils/createSvgIcon';
 
 export default createSvgIcon(
-  <>
-    <path d="M8.25 21.75H15.75" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
-    <path d="M12 21.75V15.75" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
-    <path
-      d="M17.9999 10.5C17.9999 8.12625 17.9962 4.18922 17.9943 3C17.9943 2.80109 17.9153 2.61032 17.7746 2.46967C17.634 2.32902 17.4432 2.25 17.2443 2.25L6.75414 2.26219C6.55563 2.26219 6.36522 2.34088 6.22464 2.48103C6.08405 2.62117 6.00476 2.81134 6.00414 3.00984C6.00414 4.44328 5.99805 9.06469 5.99805 10.5C5.99805 13.5131 9.88867 15.75 11.998 15.75C14.1074 15.75 17.9999 13.5131 17.9999 10.5Z"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M6 4.5H2.25V5.25C2.25 7.83844 3.82266 10.5 6 10.5"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M18 4.5H21.75V5.25C21.75 7.83844 20.1773 10.5 18 10.5"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </>
+    <>
+        <path d="M8.25 21.75H15.75" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M12 21.75V15.75" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+        <path
+            d="M17.9999 10.5C17.9999 8.12625 17.9962 4.18922 17.9943 3C17.9943 2.80109 17.9153 2.61032 17.7746 2.46967C17.634 2.32902 17.4432 2.25 17.2443 2.25L6.75414 2.26219C6.55563 2.26219 6.36522 2.34088 6.22464 2.48103C6.08405 2.62117 6.00476 2.81134 6.00414 3.00984C6.00414 4.44328 5.99805 9.06469 5.99805 10.5C5.99805 13.5131 9.88867 15.75 11.998 15.75C14.1074 15.75 17.9999 13.5131 17.9999 10.5Z"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+        <path
+            d="M6 4.5H2.25V5.25C2.25 7.83844 3.82266 10.5 6 10.5"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            fill="none"
+        />
+        <path
+            d="M18 4.5H21.75V5.25C21.75 7.83844 20.1773 10.5 18 10.5"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            fill="none"
+        />
+    </>
 );
